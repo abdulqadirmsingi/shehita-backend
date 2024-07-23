d4e55c31100fbbdc64bcf8c575cbd4746e49879a
Abdulqadir Mohammed <qadirmsingi007@gmail.com>
2024-07-23 23:40:27 +0300

    user authentication

--- shehita/myusers/urls.py
+++ shehita/myusers/urls.py
@@ -6,4 +6,6 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     # path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('refresh/', jwt_views.TokenRefreshView.as_view(), name='refresh'),
+    
+
 ]
